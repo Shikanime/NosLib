4b51c0ee601381fd9548a1a698404f8c9238fcad
Shikanime Deva <deva.shikanime@protonmail.com>
2019-03-10 23:48:41 +0100

    Initial commit

---
+++ lib/noslib/packets/username_packet.ex
@@ -0,0 +1,18 @@
+defmodule NosLib.UsernamePacket do
+  @moduledoc """
+  First UsernameRequest packet.reply by the client
+  """
+
+  defstruct user_name: nil
+
+  @type t :: %__MODULE__{
+          user_name: String.t(),
+        }
+
+  @spec parse(binary) :: t
+  def parse(payload) do
+    %__MODULE__{
+      user_name: payload
+    }
+  end
+end
